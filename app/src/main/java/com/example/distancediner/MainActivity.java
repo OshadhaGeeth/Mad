0b3ccec55222a862529525b96e7266f1a569d6a2
OshadhaGeeth <oshadaclans@gmail.com>
2022-04-27 00:35:41 +0530

    Created project

---
+++ app/src/main/java/com/example/distancediner/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.distancediner;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Profit Calculator");
+        //actionBar.setSubtitle("Insert your details to calculate");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public void btnGo(View view){
+        Intent intent= new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
