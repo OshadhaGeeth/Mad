ce7bf2c1435e5888a50c086311b1544ae3b3ce67
Sanjana Kavindi <it20659912@my.sliit.lk>
2022-05-07 23:43:10 +0530

    MainActivity.java modified

--- app/src/main/java/com/example/distancediner/MainActivity.java
+++ app/src/main/java/com/example/distancediner/MainActivity.java
@@ -1,40 +1,67 @@
 package com.example.distancediner;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Button button;
+    EditText et1,et3,etm1;
+    Button b1;
+
+    DatabaseReference db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //button=findViewById(R.id.b1);
-        //button.setOnClickListener(new View.OnClickListener(){
-            //@Override
-            //public void onClick(View view){ nextActivity(); }
-        //});
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Add Menu");
         //actionBar.setSubtitle("Insert your details to calculate");
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        et1 = findViewById(R.id.et1);
+        et3 = findViewById(R.id.et3);
+        etm1 = findViewById(R.id.etm1);
+
+        b1 = findViewById(R.id.b1);
+
+        db = FirebaseDatabase.getInstance().getReference().child("Menues");
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                insertData();
+            }
+        });
+
+    }
+    private void insertData(){
+
+        String price = et1.getText().toString();
+        String meal = et3.getText().toString();
+        String description = etm1.getText().toString();
+
+        Menues menu = new Menues(meal, description, price);
+
+        db.push().setValue(menu);
+        Toast.makeText(MainActivity.this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
+
     }
 
-    public void nextActivity(View view){
-        Intent intent= new Intent(this, MainActivity3.class);
-        startActivity(intent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
