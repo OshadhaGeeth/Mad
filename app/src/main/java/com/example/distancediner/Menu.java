37265515a917364f57a8c35e288ef4f3cc90c21c
Sanjana Kavindi <it20659912@my.sliit.lk>
2022-05-07 20:45:40 +0530

    menu.java implemented

--- app/src/main/java/com/example/distancediner/Menu.java
+++ app/src/main/java/com/example/distancediner/Menu.java
@@ -1,4 +1,40 @@
 package com.example.distancediner;
 
 public class Menu {
+
+    String meal;
+    String description;
+    String price;
+
+
+    public Menu(String meal, String description, String price) {
+        this.meal = meal;
+        this.description = description;
+        this.price = price;
+
+    }
+
+    public String getMeal() {
+        return meal;
+    }
+
+    public void setMeal(String meal) {
+        this.meal = meal;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
