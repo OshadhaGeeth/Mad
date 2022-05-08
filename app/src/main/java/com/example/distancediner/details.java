4d13f6ca11ebb43226f68f648d7021e30da7eaef
adithya <adithyapanchali20@gmail.com>
2022-05-08 19:46:06 +0530

    create details.java

---
+++ app/src/main/java/com/example/distancediner/details.java
@@ -0,0 +1,38 @@
+package com.example.distancediner;
+
+public class details {
+
+    String name;
+    String contactNumber;
+    String address;
+
+    public details(String name, String contactNumber, String address) {
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
