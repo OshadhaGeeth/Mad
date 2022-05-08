96638317dd297acd1d5b94462bf1558c84b91780
Sanjana Kavindi <it20659912@my.sliit.lk>
2022-05-08 21:58:52 +0530

    list.java created

---
+++ app/src/main/java/com/example/distancediner/List.java
@@ -0,0 +1,21 @@
+package com.example.distancediner;
+
+public class List {
+    String name, address, contact, noOfParcels;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getNoOfParcels() {
+        return noOfParcels;
+    }
+}
